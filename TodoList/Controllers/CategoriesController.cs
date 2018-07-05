c105a150b8216d8c4be9a6642454d8987f033ed3
Nabil <abilaz24@gmail.com>
2018-07-05 12:58:57 +0200

    methode CRUD de categories (get put delete dans Init)

--- TodoList/Controllers/CategoriesController.cs
+++ TodoList/Controllers/CategoriesController.cs
@@ -25,9 +25,49 @@ namespace TodoList.Controllers
             db.Categories.Add(category);
             db.SaveChanges();
 
-            return Ok();
+            return CreatedAtRoute("DefaultApi", new { id = category.ID }, category);
         }
-       
+
+        [ResponseType(typeof(Category))]
+        public List<Category> GetCategory()
+        {
+            return (from x in db.Categories select x).ToList();
+        }
+
+        [ResponseType(typeof(Category))]
+        public IHttpActionResult GetCategory(int id)
+        {
+            var test = db.Categories.SingleOrDefault(x => x.ID == id);
+            return Ok(test);
+        }
+
+        [ResponseType(typeof(Category))]
+        public IHttpActionResult PutCategory(int id, Category category)
+        {
+            if (id != category.ID)
+            {
+                return BadRequest();
+            }
+            var elementAMod = db.Categories.SingleOrDefault(x => x.ID == id);
+            elementAMod.Name = category.Name;
+            db.SaveChanges();
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        [ResponseType(typeof(Category))]
+        public IHttpActionResult DeleteCategory(int id)
+        {
+            var test = db.Categories.SingleOrDefault(x => x.ID == id);
+            if (test == null)
+                return BadRequest();
+            db.Categories.Remove(test);
+            db.SaveChanges();
+
+
+            return Ok(test);
+        }
+
         //réecriture de la methode dispose pour libérer en mémoire le DbContext et donc la connexion
         // override utilisé pour réecriture
         // methode dispose appelée lorsque que IIS n'utilise plus le controller
